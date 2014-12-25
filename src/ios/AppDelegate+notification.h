1b45105723c4fdaeee000393b10b5da80dd6c6d1
xieyilong <yilongxie@qq.com>
2014-12-25 11:41:12 +0800

    ios: 添加百度云推送 插件依赖项
    项目生成后去掉：AppDelegate+notification.h .m 的注释
    添加：libBpush.a 的framework 的引用

--- src/ios/AppDelegate+notification.h
+++ src/ios/AppDelegate+notification.h
@@ -1,20 +1,26 @@
-//
-//  AppDelegate+notification.h
-//  pushtest
-//
-//  Created by Robert Easterday on 10/26/12.
-//
-//
+// //
+// //  AppDelegate+notification.h
+// //  pushtest
+// //
+// //  Created by Robert Easterday on 10/26/12.
+// //
+// //
 
-#import "AppDelegate.h"
+// #import "AppDelegate.h"
 
-@interface AppDelegate (notification)
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
-- (void)applicationDidBecomeActive:(UIApplication *)application;
-- (id) getCommandInstance:(NSString*)className;
+// @interface AppDelegate (notification)
+// - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+// - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
+// - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
+// - (void)applicationDidBecomeActive:(UIApplication *)application;
+// - (id) getCommandInstance:(NSString*)className;
 
-@property (nonatomic, retain) NSDictionary	*launchNotification;
+// @property (nonatomic, retain) NSDictionary	*launchNotification;
 
-@end
+// //@property (nonatomic, retain) NSMutableDictionary	*idDic;
+// //@property (nonatomic, strong) NSString *appId;
+// //@property (nonatomic, strong) NSString *channelId;
+// //@property (nonatomic, strong) NSString *userId;
+
+
+// @end
